a4d3d0900ee9baacd35cd5473b04ae14fb8a2c34
dai0204 <nandai0204@gmail.com>
2019-07-01 17:34:22 +0900

    migrate

--- db/migrate/20181011013328_add_index_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_index:users, :email, unique:true
-    add_index:users, :name
-  end
-end
--- db/migrate/20181012005254_add_index_to_tasks_user_id.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToTasksUserId < ActiveRecord::Migration[5.2]
-  def change
-    add_index:tasks, :user_id
-  end
-end
